f77104f20f7e96476c6c9dbc250604bb211596bd
Lucas Rocha <lucasrocharsx@outlook.com>
2022-03-27 23:16:15 -0300

    Lambdas

---
+++ src/dvslambdas/Program.java
@@ -0,0 +1,43 @@
+package dvslambdas;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+
+import dvslambdas.entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> produtos = new ArrayList<>();
+		String path = "C:\\\\Users\\\\Lucas Rocha\\\\Documents\\\\produto.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String linha = br.readLine();
+			while (linha != null) {
+				String[] linhas = linha.split(",");
+				produtos.add(new Product(linhas[0], Double.parseDouble(linhas[1]), Integer.parseInt(linhas[2])));
+				linha = br.readLine();
+			}
+
+			produtos.sort(new Comparator<Product>(){
+				@Override
+				public int compare(Product p1, Product p2) {
+					return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (Product p : produtos) {
+			System.out.println(p);
+		}
+	}
+}
